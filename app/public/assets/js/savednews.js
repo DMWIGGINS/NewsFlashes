76ea0fe42be775689fb2bfb76784e0424d83055f
Doreen Wiggins <Wiggy77@aol.com>
2018-02-12 01:43:13 -0500

    save point

--- app/public/assets/js/savednews.js
+++ app/public/assets/js/savednews.js
@@ -1,18 +1,21 @@
 // $(document).on('click', '#deleteNewsBtn', function (event) {
+// console.log("deleteNewBtn clicked");
+
 //     // Make sure to preventDefault on a submit event.
-  
-    
+
+
 // });
 
 
 // $(document).on('click', '#addNotesBtn', function (event) {
+// console.log("addNotesBtn clicked");
 //     // Make sure to preventDefault on a submit event.
-  
+
 
 //   });
 
 //   $(document).on('click', '#deleteNotesBtn', function (event) {
+// console.log("deleteNotesBtn clicked");
 //     // Make sure to preventDefault on a submit event.
-  
+
 //   });
-  
--- models/Newsflash.js
+++ models/Newsflash.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+
+
 var NewsflashSchema = new Schema({
     // `title` is required and of type String
     title: {
@@ -23,12 +23,11 @@ var NewsflashSchema = new Schema({
 // 'saved' is required and of type Boolean
     saved: {
         type: Boolean,
-        required: true,
         default: false
     },
     // `note` is an object that stores a Note id
     // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
+    // This allows us to populate the Newsflash with an associated Note
     notes: {
         type: Schema.Types.ObjectId,
         ref: "Notes"
