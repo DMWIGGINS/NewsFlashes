95c79f17e84f738cfac3f5dbc44607b65f05b12e
Doreen Wiggins <Wiggy77@aol.com>
2018-02-12 20:05:26 -0500

    moved view folder into the correct place

--- app/controller/news_controller.js
+++ app/controller/news_controller.js
@@ -11,23 +11,24 @@ function getNewsFlashes(req, res) {
     // Grab every document in the Newsflash collection
     db.Newsflash.find({})
 
+
+
         .then(function (dbNewsflash) {
             // If we were able to successfully find Newsflashes, send them back to the client
 
            
 
-            res.json(dbNewsflash);
-            console.log("this is dbNewsFlash in getNewsFlashes" + dbNewsflash);
-            console.log("this is newsFlash in getNewsFlashes" + newsFlash);
+            res.render("index",dbNewsflash);
+            // console.log("this is dbNewsFlash in getNewsFlashes" + dbNewsflash);
+        
 
         })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
+        // .catch(function (err) {
+        //     // If an error occurred, send it to the client
+        //     res.json(err);
+        // });
     console.log("got NewsFlashes");
 
-    // res.render("index", newsFlash);
 };
 
 function getSavedNews(req, res) {
@@ -38,26 +39,26 @@ function getSavedNews(req, res) {
 router.get("/scrape", function (req, res) {
     console.log("I'm in scrape in the controller");
     request("https://www.usatoday.com/news/world/", function (error, response, html) {
-
+        
         // Load the HTML into cheerio and save it to a variable
         // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
         var $ = cheerio.load(html);
-
+        
         // An empty array to save the data that we'll scrape
-
-        // chose the element span with class hgpm-list-hed as a starting point to be able to access all needed article info
+        
+        // choose the element span with class hgpm-list-hed as a starting point to be able to access all needed article info
         $("span.hgpm-list-hed").each(function (i, element) {
-
+            
             var results = [];
             // Save the text of the element in a "title" variable
             var title = $(element).text();
-
+            
             // Save the article summary in a "summary" variable
             var summary = $(element).next().attr("data-fulltext");
-
+            
             // accessing the link to the article by the "href" attribute of it's great grandparents
             var link = $(element).parent().parent().parent().attr("href");
-
+            
             // Save these results in an object that we'll push into the results array we defined earlier
             results.push({
                 title: title,
@@ -65,24 +66,25 @@ router.get("/scrape", function (req, res) {
                 link: link,
                 saved: false
             });
-
+            
             // sending the results to the database
             db.Newsflash.create(results)
-
-                .then(function (dbNewsflash) {
-                    // View the added result in the console
-                    console.log("dbNewsFlash after scraping and adding to the database" + dbNewsflash);
-                })
-                .catch(function (err) {
-                    // If an error occurred, send it to the client
-                    return res.json(err);
-                });
+            
+            .then(function (dbNewsflash) {
+                // View the added result in the console
+                console.log("dbNewsFlash after scraping and adding to the database" + dbNewsflash);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                return res.json(err);
+            });
         });
         // Log the results once you've looped through each of the elements found with cheerio
-
+        
         res.send("Scrape Complete");
-        // getNewsFlashes(req, res);
-        // res.render("index", newsFlash );
+        
+        getNewsFlashes(res);
+    
     });
 });
 
@@ -91,12 +93,12 @@ router.get("/", function (req, res) {
 });
 // Default route and route for getting the Newsflashes from the database
 // router.get("/", function (req, res) {
-//     getNewsflashes(req, res);
-// });
-
-router.get("/newsflashes", function (req, res) {
-    getNewsFlashes(req, res);
-});
+    //     getNewsflashes(req, res);
+    // });
+    
+    router.get("/newsflashes", function (req, res) {
+        getNewsFlashes(req, res);
+    });
 
 // Route for the savednews page 
 router.get("/savednews", function (req, res) {
