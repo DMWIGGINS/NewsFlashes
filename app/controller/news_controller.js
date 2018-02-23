b8f1fee9879aa0817f02a9e5c3673ba4bc9e4424
Doreen Wiggins <Wiggy77@aol.com>
2018-02-23 14:32:15 -0500

    fixing addNote

--- app/controller/news_controller.js
+++ app/controller/news_controller.js
@@ -303,14 +303,15 @@ router.post("/api/notes/:id", function (req, res) {
             console.log("dbNotes after posting new note to the database" + dbNotes);
             console.log("dbNotes_id is " + dbNotes._id);
             console.log(req.params.id);
-            var note = {"notes": dbNotes._id};
+            // var note = {"notes": dbNotes._id};
         })
     return db.Newsflash.findOneAndUpdate({
             _id: req.params.id
         }, {
-            $push: {
-                notes: note
-            }
+            // $push: {
+            // notes: note
+            // }
+            notes: dbNotes_id
         })
         .then(function (dbNewsflash) {
 
